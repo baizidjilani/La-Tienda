92dab2394b275abf3f69938555d438593a31c9f5
bracealround <souravsaha0152@gmail.com>
2022-08-27 01:03:47 +0600

    cart added

--- frontend/src/components/ProductDescription.js
+++ frontend/src/components/ProductDescription.js
@@ -14,12 +14,35 @@ export default function ProductDescription() {
         setDescription(data);
         setIsLoading(false);
     };
+    const addItem = (product) => {
+        let cartArray = localStorage.getItem("cart");
+        if (cartArray === null) {
+          localStorage.setItem("cart", JSON.stringify([{ ...product, inCart: 1 }]));
+        } else {
+          let updatedCart = JSON.parse(cartArray);
+    
+          if ( updatedCart.some((item)=> item._id === product._id)){
+            updatedCart.map((item, index) => {
+                if (item._id === product._id) {
+                 return updatedCart[index].inCart += 1;
+                }
+              });
+    
+          }
+          else {
+            updatedCart.push({...product, inCart: 1})
+          }
+          
+          localStorage.setItem("cart",JSON.stringify(updatedCart));
+        }
+      };
+
     return (
         <section className="my-4">
            <div className="container">
             <div className="row">
                 <div className="col-md-6">
-                    <img src="./images/watch.jpg" alt="image" className="w-100"/>
+                    <img src="./images/watch.jpg" alt="product" className="w-100"/>
                 </div>
                 <div className="col-md-6">
                     <div>
@@ -27,7 +50,7 @@ export default function ProductDescription() {
                         <p>{description.desc}</p>
                         <hr />
                         <p className="display-6">$ {description.price}</p>
-                        <button type="button" className="btn btn-outline-dark w-100">Add to cart</button>
+                        <button type="button" className="btn btn-outline-dark w-100" onClick={() => addItem(description)}>Add to cart</button>
                     </div>
                 </div>
             </div>
--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,48 +1,85 @@
 import Banners from "./Banners";
 import { useEffect, useState } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Products() {
-    const [products, setProducts] = useState([])
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        fetchData();
-    }, []);
-    const fetchData = async () => {
-        const res = await fetch('http://localhost:5000/api/products');
-        const data = await res.json();
-        console.log(data);
-        setProducts(data);
-        setIsLoading(false);
-    };
-    return (
-        <>
-            <Banners />
-            <h1 className="text-dark text-center my-5">Products</h1>
-            <section className="mb-5">
-                <div className='container'>
-                    <div className="row row-cols-1 row-cols-md-3 g-4">
-                        {
-                            products.map(items => {
-                                return (
-                                    <div className="col" key={items._id}>
-                                        <Link to = {`/${items._id}`}>
-                                            <div className="card h-100" style={{ 'padding': '0px' }} id ='product-card'>
-                                                <img src='./images/watch.jpg' className="card-img-top" alt={items.img} />
-                                                <div className="card-body">
-                                                    <h5 className="card-title fw-bolder text-dark">{items.title}</h5>
-                                                    <p className="card-text fw-bold text-dark">Price : $ {items.price}</p>
-                                                    <button type="button" className="btn btn-outline-dark w-100">Add to cart</button>
-                                                </div>
-                                            </div>
-                                        </Link>
-                                    </div>
-                                )
-                            })
-                        }
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const res = await fetch("http://localhost:5000/api/products");
+    const data = await res.json();
+    setProducts(data);
+    setIsLoading(false);
+  };
+  const addItem = (product) => {
+    let cartArray = localStorage.getItem("cart");
+    if (cartArray === null) {
+      localStorage.setItem("cart", JSON.stringify([{ ...product, inCart: 1 }]));
+    } else {
+      let updatedCart = JSON.parse(cartArray);
+
+      if ( updatedCart.some((item)=> item._id === product._id)){
+        updatedCart.map((item, index) => {
+            if (item._id === product._id) {
+             return updatedCart[index].inCart += 1;
+            }
+          });
+
+      }
+      else {
+        updatedCart.push({...product, inCart: 1})
+      }
+      
+      localStorage.setItem("cart",JSON.stringify(updatedCart));
+    }
+  };
+  return (
+    <>
+      <Banners />
+      <h1 className="text-dark text-center my-5">Products</h1>
+      <section className="mb-5">
+        <div className="container">
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {products.map((items) => {
+              return (
+                <div className="col" key={items._id}>
+                  <div
+                    className="card h-100"
+                    style={{ padding: "0px" }}
+                    id="product-card"
+                  >
+                    <Link to={`/${items._id}`}>
+                      <img
+                        src="./images/watch.jpg"
+                        className="card-img-top"
+                        alt={items.img}
+                      />
+                    </Link>
+                    <div className="card-body">
+                      <h5 className="card-title fw-bolder text-dark">
+                        {items.title}
+                      </h5>
+                      <p className="card-text fw-bold text-dark">
+                        Price : $ {items._id}
+                      </p>
+                      <button
+                        type="button"
+                        className="btn btn-outline-dark w-100"
+                        onClick={() => addItem(items)}
+                      >
+                        Add to cart
+                      </button>
                     </div>
+                  </div>
                 </div>
-            </section>
-        </>
-    )
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
